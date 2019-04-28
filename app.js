5e8b250202c15e4d17b9f3a52e12caffaf40858e
vietdinh17 <vietqudinh@gmail.com>
2019-04-27 20:23:49 -0700

    Implemented Wit.ai

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
-import { twil } from './config/secret';
+import { twil, wit } from './config/secret';
 
+// create twilio instances 
 const client = require('twilio')(twil.SID, twil.Token);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+// create wit
+const { Wit, log } = require('node-wit');
 
 // create our instances
 const app = express();
@@ -14,31 +18,59 @@ const router = express.Router();
 const API_PORT = process.env.API_PORT || 3001;
 
 // now we should configure the API to use bodyParser and look for JSON data in the request body
-app.use(bodyParser.urlencoded( {extended:false} ));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
 
 // now we can set the route path & initialize the API
 router.get('/', (req, res) => {
-    res.json({ message: 'Hello, World!'});
-    client.messages
-    .create({
-        body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-        from: '+18317095768',
-        to: '+14087992849'
-    })
-    .then(message => console.log(message.sid))
+  res.json({ message: 'Hello, World!' });
+  // client.messages
+  // .create({
+  //     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+  //     from: '+18317095768',
+  //     to: '+14087992849'
+  // })
+  // .then(message => console.log(message.sid))
+
+  // const witClient = new Wit({ accessToken: wit.Token });
+  // witClient.message('What is the temperature in here?', {})
+  //   .then((data) => {
+  //     console.log('Yay, got Wit.ai response: ' + data.entities.intent[0].value);
+  //   })
+  //   .catch(console.error);
+
 });
 
 // test reponse
 router.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
-  
-  twiml.message('The Robots are coming! Head for the hills!');
+  const witClient = new Wit({ accessToken: wit.Token });
+  witClient.message(req.body.Body, {})
+    .then((data) => {
+      let intent = data.entities.intent ? data.entities.intent[0].value : 'empty';
+      console.log('Yay, got Wit.ai response: ' + intent);
+      if (intent == 'food') {
+        twiml.message('You want some food?');
+      }else if (req.body.Body == 'Hello') {
+        twiml.message('Hi!');
+      } else if (req.body.Body == 'Bye') {
+        twiml.message('Goodbye');
+      }
+      res.writeHead(200, { 'Content-Type': 'text/xml' });
+      res.end(twiml.toString());
+    })
+    .catch(console.error);
 
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+  // else {
+  //   twiml.message(
+  //     'No Body param match, Twilio sends this in the request to your server.'
+  //   );
+  // }
+
+  // res.writeHead(200, { 'Content-Type': 'text/xml' });
+  // res.end(twiml.toString());
 });
 
 // Use our router configuration when we call /api
--- config/secret.js
+++ config/secret.js
@@ -2,3 +2,7 @@ export const twil = {
     "SID":"AC8ca3755134f7133e90cd4d0ef717d035",
     "Token":"a87fb7cd4384986ff477ef0b0bd2abba",
 }
+
+export const wit = {
+    "Token" : "COGJZGBTXZOWQJIXVWXV6LI3QRDN5DZI"
+}
